73196728299b1f727f0a30734da540935b7f41d6
PrismSpirit <yeolkeut@gmail.com>
2022-01-19 15:54:34 +0900

    Add model to Token and History

--- database/models.py
+++ database/models.py
@@ -10,3 +10,14 @@ class User(Base):
     passphrase = Column(String)
     user_wallet = Column(String)
     user_type = Column(String, nullable=False)
+
+class Token(Base):
+    __tablename__ = "Token"
+
+    token_id = Column(Integer, primary_key=True, nullable=False)
+
+class History(Base):
+    __tablename__ = "History"
+
+    token_id = Column(Integer, nullable=False)
+    tracking = Column(String, nullable=False)
