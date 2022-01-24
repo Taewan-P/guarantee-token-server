266db723f48837f8d7730aaf673faba8f95dc6cb
PrismSpirit <yeolkeut@gmail.com>
2022-01-24 18:30:12 +0900

    Add Token Class for models

--- database/models.py
+++ database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, Date
 from database.DB import Base
 
 
@@ -16,6 +16,11 @@ class Token(Base):
     __tablename__ = "Token"
 
     token_id = Column(Integer, primary_key=True, nullable=False)
+    logo = Column(String, nullable=False)
+    brand = Column(String, nullable=False)
+    production_date = Column(Date, nullable=False)
+    expiration_date = Column(Date, nullable=False)
+    details = Column(String, nullable=False)
 
 
 class History(Base):
