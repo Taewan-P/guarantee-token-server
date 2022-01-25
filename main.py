399619d6e938d4657523a58c0390d8668bf4bb0c
Taewan Park <swimingtw@gmail.com>
2022-01-25 17:06:23 +0900

    Add files for heroku deployment

--- main.py
+++ main.py
@@ -10,7 +10,3 @@ app = FastAPI()
 app.include_router(account_router, prefix='/account')
 app.include_router(node_router, prefix='/node')
 app.include_router(token_router, prefix='/tokens')
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
